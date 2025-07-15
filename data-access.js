65dbf1016874b561015496963a9fcccc9e3a7dde
C W <westfall.chris@icloud.com>
2025-07-15 16:39:50 -0400

    Stage 02 - GET /customers from MongoDB

---
+++ data-access.js
@@ -0,0 +1,26 @@
+// data-access.js
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb://localhost:27017";
+const dbName = "custdb";
+let collection;
+
+async function connect() {
+  const client = new MongoClient(url);
+  await client.connect();
+  const db = client.db(dbName);
+  collection = db.collection("customers");
+}
+
+async function getCustomers() {
+  try {
+    if (!collection) await connect();
+    const customers = await collection.find().toArray();
+    return [customers, null];
+  } catch (err) {
+    console.error(err.message);
+    return [null, err.message];
+  }
+}
+
+module.exports = { getCustomers };
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-// server.js
 const express = require("express");
 const path = require("path");
+const da = require("./data-access");
 
 const app = express();
 const port = 4000;
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Start the server
+app.get("/customers", async (req, res) => {
+  const [cust, err] = await da.getCustomers();
+  if (cust) {
+    res.send(cust);
+  } else {
+    res.status(500).send(err);
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
